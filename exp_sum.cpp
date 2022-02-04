b06feecc3f07fcd6766c080ec4ee7cc5477aea5c
Ty Klabacka <86436972+Tklaback@users.noreply.github.com>
2022-02-03 18:34:02 -0700

    Initial commits

---
+++ exp_sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using ull = unsigned long long;
+
+ull exp_sum(unsigned int n) {
+  
+  return 0;
+}
+
+int main(){
+    exp_sum(10);
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+istringstream converter("05");
+    // int x;
+    // converter >> x;
+    // cout << x * 5 << endl;
